5c9b0a1fb159ccfa08e9156c19206722aaf01c70
Denis Kitrish <denis.kitrish@outlook.com>
2023-04-09 15:49:45 +0300

    1. Определите тип данных для переменных. [#172634]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -2,6 +2,24 @@ package ru.job4j;
 
 public class Info {
     public static void main(String[] args) {
-        System.out.println("05.04.2023");
+
+        byte age1 = 32;
+        System.out.println(age1);
+        char gender = 'F';
+        System.out.println(gender);
+        float weight = 60.2F;
+        System.out.println(weight);
+        float height = 1.70F;
+        System.out.println(height);
+        short bloks = 10000;
+        System.out.println(bloks);
+        int bacterium = 10000000;
+        System.out.println(bacterium);
+        long to_mars = 55000000L;
+        System.out.println(to_mars);
+        boolean job = true;
+        System.out.println(job);
+        byte size = 3;
+        System.out.println(size);
     }
-}
+    }
